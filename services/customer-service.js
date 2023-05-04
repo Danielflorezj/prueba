3457ba1a289dd1a916a45bbf90fc7307551011e8
danielflorezj <dflorez507@gmail.com>
2023-05-04 13:35:10 -0500

    guardado de clientes

--- services/customer-service.js
+++ services/customer-service.js
@@ -2,9 +2,9 @@ import CustomerModel from "../models/customer-model.js"
 
 const URL_API ="https://645284acbce0b0a0f74922d6.mockapi.io/"
 
-const refRegistro = document.querySelector('#registro');
-const refListar = document.querySelector('#listar');
-const refBuscar = document.querySelector('#buscar');
+const frmRegistro = document.querySelector('#frmData');
+const inputFrm = document.forms['frmData'];
+const botones = document.querySelector('.btn');
 
 const myHeaders = new Headers({
     "content-Type": "application/json"
@@ -49,7 +49,7 @@ function saveCustomer(){
     CustomerModel.createdAt= '2023-02-02'
     CustomerModel.nombre = 'campers 2023'
     CustomerModel.apellidos = 'xxxx'
-    CustomerModel.fechaNacimiento = 'xxx'
+    CustomerModel.fechaNacimiento = '199-09-08'
     postCustomer(CustomerModel);
 
 }
@@ -57,19 +57,52 @@ function verOcultar(divsVisible){
     console.log(divsVisible);
 
 }
-refListar.addEventListener("click",getCustomers);
-refRegistro.addEventListener("click",(e)=>{
-    verOcultar(['#reg',['#listar','#buscar']]);
-    e.preventDefault();
-    e.stopImmediatePropagation();
-});
-document.querySelectorAll('.tapOpcion').forEach((val.id) =>{
+document.querySelectorAll('.tabOpcion').forEach((val,id) =>{
     val.addEventListener("click",(e)=>{
-        let datos = JSON.parce(e.target.dataset.verOcultar);
+        let datos = JSON.parse(e.target.dataset.verocultar);
+        let cardVer = document.querySelector(datos[0]);
+        if (cardVer.id == '#reg')
+        cardVer.style.display = 'block';
+        datos[1].forEach(card => {
+            let cardActual = document.querySelector(card);
+            cardActual.style.display = 'none';
+        });
+        e.stopImmediatePropagation();
+        e.preventDefault();
     })
 });
-
 
 function viewDaraHtml(dataCustomer){
     console.log(dataCustomer);
 }
+document.querySelector('#btnNuevo').addEventListener("click",(e) =>{
+    inputFrm.querySelectorAll('.form-control').forEach((e) =>{
+        e.value = '';
+        if (e.name == 'createdAt'){
+            e.valueAsDate = new Date();
+            e.disabled = true;
+        }
+       
+    })
+    document.querySelectorAll('.btn').forEach((element) =>{
+        console.log(element);
+        element.disabled=true;
+        if((element.id != 'btnGuardar') && (element.id != 'btnCancelar')){
+            element.classList.add('disabled');
+        }
+    })
+    document.querySelectorAll('.btn').forEach((element) =>{
+        element.disabled = true;
+        if((element.id !='btnGuardar')){
+            element.classList.add('disabled');
+        }
+    })
+})
+document.querySelector('#btnGuardar').addEventListener("click", (e) =>{
+    const datos = Object.fromEntries(new FormData(frmRegistro).entries());
+    console.log(datos);
+    postCustomer(datos)
+})
+/* function ActibarBtn(){
+
+} */
